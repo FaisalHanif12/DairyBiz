8bb8ea86ca38c82713f74e5f96f5932d601a2553
Faisal Hanif <mehrfaisal111@example.com>
2024-09-23 14:55:16 -0700

    refine the login page

--- my-frontend/src/LoginForm.jsx
+++ my-frontend/src/LoginForm.jsx
@@ -1,33 +1,34 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './LoginForm.css'; // Make sure the path is correct
+import './LoginForm.css'; // Importing CSS for styling
 
 function LoginForm({ onLogin }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [showAlert, setShowAlert] = useState(false);
-  const navigate = useNavigate();
+  const [username, setUsername] = useState(''); // State for username
+  const [password, setPassword] = useState(''); // State for password
+  const [showAlert, setShowAlert] = useState(false); // State to manage alert visibility
+  const navigate = useNavigate(); // Hook for navigation
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    const storedUsername = 'Waiz';
-    const storedPassword =  'Waiz92';
+    event.preventDefault(); // Prevent default form submission behavior
+    const storedUsername = 'Waiz'; // Stored username for comparison
+    const storedPassword = 'Waiz92'; // Stored password for comparison
+    console.log("Attempting to login with username:", username); // Console log the attempt
     if (username === storedUsername && password === storedPassword) {
-      onLogin();
-      navigate('/');
+      onLogin(); // Callback function on successful login
+      navigate('/'); // Navigate to the home page
     } else {
-      setShowAlert(true);
+      setShowAlert(true); // Show alert on failed login attempt
       setTimeout(() => setShowAlert(false), 3000); // Hide the alert after 3 seconds
     }
   };
 
   return (
     <div className="login-form-container">
-      <div className="background-text">Maher Dairy Farm</div> {/* Background text */}
+      <div className="background-text">Maher Dairy Farm</div> {/* Background branding text */}
       <div className="login-form-card">
         {showAlert && (
           <div className="alert-message show">
-            Incorrect username or password!
+            Incorrect username or password! {/* Alert message */}
           </div>
         )}
         <form onSubmit={handleSubmit} className="login-form">
